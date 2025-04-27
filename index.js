e49102117c8aabef59eb634ac5e4a24c059345db
smlimonparvez <smlimonparvez@gmail.com>
2025-04-28 03:31:39 +0600

    comment all unnecesary code

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
+require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const app = express();
-require("dotenv").config();
 const port = process.env.PORT || 5000;
 
 // middleware
@@ -23,12 +23,12 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
     // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log(
-      "Pinged your deployment. You successfully connected to MongoDB!"
-    );
+    // await client.db("admin").command({ ping: 1 });
+    // console.log(
+    //   "Pinged your deployment. You successfully connected to MongoDB!"
+    // );
 
     const visaCollection = client.db("visaDB").collection("visas");
     const visaApplicationsCollection = client
